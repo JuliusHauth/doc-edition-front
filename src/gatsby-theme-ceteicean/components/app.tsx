bad08c0dd4ef3020fa0edbfff6a42f80b42830a9
JuliusHauth <juliushauth@outlook.de>
2024-07-07 20:32:18 +0200

    add reading functionality

--- src/gatsby-theme-ceteicean/components/app.tsx
+++ src/gatsby-theme-ceteicean/components/app.tsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { Behavior } from "gatsby-theme-ceteicean/src/components/Behavior"
-import { TEINodes } from "react-teirouter"
+import { TEINodes, TEINode } from "react-teirouter"
+import { Popover } from "@mui/material"
 
 interface TEIProps {
     teiNode: Node
@@ -9,13 +10,28 @@ interface TEIProps {
   
 
 const App = ({teiNode, availableRoutes}: TEIProps) => {
+    const [hovered, setHovered] = useState(false)
+    const appEl = teiNode as Element
+    const main = appEl.querySelector('tei-lem') || appEl.querySelector('tei-rdg')
+    const other = appEl.querySelectorAll('tei-rdg')
+
+    console.log(main, other, teiNode)
+
+    if (!main || !other) return null
+
     return (
         <Behavior node={teiNode}>
             <span className="app">
-                <TEINodes 
-                teiNodes={teiNode.childNodes} 
-                availableRoutes={availableRoutes} />
-            
+                <span className='main' onClick={() => setHovered(true)} >
+                    <TEINode 
+                        teiNode={main} 
+                        availableRoutes={availableRoutes} />
+                </span>
+                <span style={{display: hovered? 'block' : 'none'}}>
+                    <TEINodes 
+                        teiNodes={other} 
+                        availableRoutes={availableRoutes} />
+                </span>
             </span>
         </Behavior>
     )
